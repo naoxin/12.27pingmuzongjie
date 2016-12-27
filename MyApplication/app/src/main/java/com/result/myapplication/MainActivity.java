84d88d1c0bd97499f284bfe82e36afb8f7dc921a
naoxin <614366778@qq.com>
2016-12-27 22:25:13 +0800

    pingmushipei

---
+++ MyApplication/app/src/main/java/com/result/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.result.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * autour: 李延
+ * date: 2016/12/27 18:32
+ * update: 2016/12/27
+ * 限定符适配
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
